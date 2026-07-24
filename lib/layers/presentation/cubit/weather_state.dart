1503a98e9c424d4cbac13ee80f4fd1b887f11609
Ruppel919 <ruppelfrec@gmail.com>
2026-07-24 17:31:39 +0500

    Cubit adding in progress

--- lib/layers/presentation/cubit/weather_state.dart
+++ lib/layers/presentation/cubit/weather_state.dart
@@ -1,17 +1,25 @@
+import 'package:equatable/equatable.dart';
 import 'package:prosto_tipo_testovui_projekt/layers/domain/entity/weather_model.dart';
 
-sealed class WeatherState {}
+sealed class WeatherState extends Equatable {
+  const WeatherState();
+  @override
+  List<Object?> get props => [];
+}
 
 final class WeatherInitial extends WeatherState {}
 
 final class WeatherLoadInProgress extends WeatherState {}
 
 final class WeatherLoadSuccess extends WeatherState {
-  WeatherLoadSuccess(Future<WeatherModel> weather);
+  const WeatherLoadSuccess(Future<WeatherModel> weather);
 }
 
 final class WeatherLoadFailure extends WeatherState {
+  @override
+  List<Object?> get props => [message];
+
   final String message;
 
-  WeatherLoadFailure(this.message);
+  const WeatherLoadFailure(this.message);
 }
--- lib/layers/presentation/pages/weather_page.dart
+++ lib/layers/presentation/pages/weather_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:prosto_tipo_testovui_projekt/layers/presentation/cubit/weather_cubit.dart';
+import 'package:prosto_tipo_testovui_projekt/layers/presentation/cubit/weather_state.dart';
 
 import 'package:prosto_tipo_testovui_projekt/layers/shared/all_widgets/bottomsheet_widget.dart';
 import 'package:prosto_tipo_testovui_projekt/layers/shared/all_widgets/burger_menu_widget.dart';
@@ -12,89 +15,99 @@ class WeatherPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          begin: AlignmentGeometry.topStart,
-          colors: [context.colors.fancyBlue, context.colors.transitionBlue],
-        ),
-      ),
-      child: Scaffold(
-        body: Stack(
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+    return BlocBuilder<WeatherCubit, WeatherState>(
+      builder: (context, state) {
+        return Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: AlignmentGeometry.topStart,
+              colors: [context.colors.fancyBlue, context.colors.transitionBlue],
+            ),
+          ),
+          child: Scaffold(
+            body: Stack(
               children: [
-                Padding(
-                  padding: EdgeInsets.only(left: 16.0),
-                  child: Text(
-                    'Погода Алматы',
-                    //title1
-                    style: context.styles.title1.copyWith(
-                      color: context.colors.white,
-                    ),
-                  ),
-                ),
-
-                HboxWidget(height: 30),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(
-                      '21°',
-                      //title2
-                      style: context.styles.title2.copyWith(
-                        color: context.colors.white,
+                    Padding(
+                      padding: EdgeInsets.only(left: 16.0),
+                      child: Text(
+                        'Погода Алматы',
+                        //title1
+                        style: context.styles.title1.copyWith(
+                          color: context.colors.white,
+                        ),
                       ),
                     ),
-                    WboxWidget(width: 25),
-                    Icon(Icons.sunny, color: context.colors.white, size: 100),
+
+                    HboxWidget(height: 30),
+                    if (state is WeatherLoadSuccess) ...[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            context.state,
+                            //title2
+                            style: context.styles.title2.copyWith(
+                              color: context.colors.white,
+                            ),
+                          ),
+                          WboxWidget(width: 25),
+                          Icon(
+                            Icons.sunny,
+                            color: context.colors.white,
+                            size: 100,
+                          ),
+                        ],
+                      ),
+                    ],
+                    HboxWidget(height: 25),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: [
+                          WboxWidget(width: 16),
+                          WeatherWidget(
+                            icon: Icons.cloud,
+                            weatherText: 'Очень облачно',
+                            weatherProcent: '55%',
+                          ),
+                          WboxWidget(width: 10),
+                          WeatherWidget(
+                            icon: Icons.cloud,
+                            weatherText: 'Очень облачно',
+                            weatherProcent: '55%',
+                          ),
+                          WboxWidget(width: 10),
+                          WeatherWidget(
+                            icon: Icons.cloud,
+                            weatherText: 'Очень облачно',
+                            weatherProcent: '55%',
+                          ),
+                          WboxWidget(width: 16),
+                        ],
+                      ),
+                    ),
                   ],
                 ),
-                HboxWidget(height: 25),
-                SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    children: [
-                      WboxWidget(width: 16),
-                      WeatherWidget(
-                        icon: Icons.cloud,
-                        weatherText: 'Очень облачно',
-                        weatherProcent: '55%',
-                      ),
-                      WboxWidget(width: 10),
-                      WeatherWidget(
-                        icon: Icons.cloud,
-                        weatherText: 'Очень облачно',
-                        weatherProcent: '55%',
-                      ),
-                      WboxWidget(width: 10),
-                      WeatherWidget(
-                        icon: Icons.cloud,
-                        weatherText: 'Очень облачно',
-                        weatherProcent: '55%',
-                      ),
-                      WboxWidget(width: 16),
-                    ],
-                  ),
-                ),
+                BottomWeatherSheet(),
               ],
             ),
-            BottomWeatherSheet(),
-          ],
-        ),
-        backgroundColor: Colors.transparent,
-        appBar: AppBar(
-          title: Text(
-            'Четверг 09/07/26',
-            //title1
-            style: TextStyle(color: context.colors.white),
+            backgroundColor: Colors.transparent,
+            appBar: AppBar(
+              title: Text(
+                'Четверг 09/07/26',
+                //title1
+                style: TextStyle(color: context.colors.white),
+              ),
+              actions: <Widget>[BurgerMenuWidget()],
+              elevation: 0,
+              backgroundColor: Colors.transparent,
+            ),
           ),
-          actions: <Widget>[BurgerMenuWidget()],
-          elevation: 0,
-          backgroundColor: Colors.transparent,
-        ),
-      ),
+        );
+      },
     );
   }
 }
